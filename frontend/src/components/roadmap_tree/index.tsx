d73499400e2ba9a4873bde3b3e82f9978cff4fc3
rb3198 <ronitbhatia@hotmail.com>
2024-09-29 09:39:40 -0400

    fix

--- frontend/src/components/roadmap_tree/index.tsx
+++ frontend/src/components/roadmap_tree/index.tsx
@@ -20,54 +20,54 @@ export const RoadmapTree = (props: Props) => {
         draggable
         data={treeData}
         collapsible={false}
-        // renderCustomNodeElement={({ nodeDatum }) => {
-        //   const { name } = nodeDatum;
-        //   return (
-        //     <g>
-        //       <circle r={15} />
-        //       <foreignObject>
-        //         <div
-        //           style={{
-        //             width: 20,
-        //             height: 20,
-        //             display: "flex",
-        //             justifyContent: "center",
-        //             alignItems: "center",
-        //             background: "blue",
-        //           }}
-        //         >
-        //           {name}
-        //         </div>
-        //       </foreignObject>
-        //     </g>
-        //     // <g>
-        //     //   {/* Render the rect with rounded corners */}
-        //     //   <rect
-        //     //     width={100}
-        //     //     height={15}
-        //     //     // x={-rectWidth / 2} // Center the rect on the node's x-axis
-        //     //     // y={-rectHeight / 2} // Center the rect on the node's y-axis
-        //     //     rx="1rem" // Border radius equivalent to 1rem
-        //     //     ry="1rem"
-        //     //     fill="#89CFF0" // Background color of the rect
-        //     //     stroke="#333" // Border color
-        //     //     strokeWidth="2"
-        //     //     // onClick={toggleNode} // Allows collapsing/expanding the node on click
-        //     //   />
+        renderCustomNodeElement={({ nodeDatum }) => {
+          const { name } = nodeDatum;
+          return (
+            <g>
+              <circle r={15} />
+              <foreignObject>
+                <div
+                  style={{
+                    width: 20,
+                    height: 20,
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    background: "blue",
+                  }}
+                >
+                  {name}
+                </div>
+              </foreignObject>
+            </g>
+            // <g>
+            //   {/* Render the rect with rounded corners */}
+            //   <rect
+            //     width={100}
+            //     height={15}
+            //     // x={-rectWidth / 2} // Center the rect on the node's x-axis
+            //     // y={-rectHeight / 2} // Center the rect on the node's y-axis
+            //     rx="1rem" // Border radius equivalent to 1rem
+            //     ry="1rem"
+            //     fill="#89CFF0" // Background color of the rect
+            //     stroke="#333" // Border color
+            //     strokeWidth="2"
+            //     // onClick={toggleNode} // Allows collapsing/expanding the node on click
+            //   />
 
-        //     //   {/* Render the name of the node centered on the rect */}
-        //     //   <text
-        //     //     x="0" // Center text horizontally
-        //     //     y="5" // Center text vertically (adjust to position text properly within the rect)
-        //     //     textAnchor="middle" // Text horizontal centering
-        //     //     alignmentBaseline="middle" // Text vertical centering
-        //     //     style={{ fontSize: "14px", fill: "#000" }} // Text styling
-        //     //   >
-        //     //     {name}
-        //     //   </text>
-        //     // </g>
-        //   );
-        // }}
+            //   {/* Render the name of the node centered on the rect */}
+            //   <text
+            //     x="0" // Center text horizontally
+            //     y="5" // Center text vertically (adjust to position text properly within the rect)
+            //     textAnchor="middle" // Text horizontal centering
+            //     alignmentBaseline="middle" // Text vertical centering
+            //     style={{ fontSize: "14px", fill: "#000" }} // Text styling
+            //   >
+            //     {name}
+            //   </text>
+            // </g>
+          );
+        }}
       />
     </>
   );
